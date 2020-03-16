8d6f854565035f3b2fe6acff917f8a259a3d2d0a
Talha Mansoor <talha131@gmail.com>
2020-03-17 00:07:17 +0500

    add code to fetch and show the result using React.useReducer() hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,135 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+type Action =
+  | { type: "typing"; value: string }
+  | { type: "fetching" }
+  | { type: "success"; payload: string }
+  | { type: "error"; code: string };
+
+interface State {
+  isFetching: boolean;
+  isSuccessful: boolean;
+  errorMessage: string;
+  result: string;
+  userIdValue: string;
+}
+
+const initialState: State = {
+  isFetching: false,
+  isSuccessful: false,
+  errorMessage: "",
+  result: "",
+  userIdValue: "1"
+};
+
+const appReducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case "fetching":
+      return {
+        ...state,
+        isFetching: true,
+        isSuccessful: false,
+        errorMessage: "",
+        result: ""
+      };
+
+    case "typing":
+      return { ...state, userIdValue: action.value };
+
+    case "success":
+      return {
+        ...state,
+        isFetching: false,
+        isSuccessful: true,
+        result: action.payload
+      };
+
+    case "error":
+      return {
+        ...state,
+        isFetching: false,
+        errorMessage: `Request failed. Error: ${action.code}`
+      };
+  }
+};
+
+const App = () => {
+  const [
+    { isSuccessful, isFetching, userIdValue, errorMessage, result },
+    dispatch
+  ] = React.useReducer(appReducer, initialState);
+
+  const fetchUserInfo = () => {
+    fetch(`https://reqres.in/api/users/${userIdValue}?delay=5`)
+      .then(response =>
+        response.status === 200
+          ? Promise.resolve(response.json())
+          : Promise.reject(response.status)
+      )
+      .then(data => {
+        dispatch({
+          type: "success",
+          payload: JSON.stringify(data, undefined, 2)
+        });
+      })
+      .catch(err => {
+        dispatch({ type: "error", code: err });
+      });
+  };
+  const onValueChanged = (event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch({ type: "typing", value: event.target.value });
+  };
+
+  const onFetchClicked = (
+    event: React.MouseEvent<HTMLInputElement, MouseEvent>
+  ) => {
+    event.preventDefault();
+    dispatch({ type: "fetching" });
+    fetchUserInfo();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form noValidate autoComplete="off">
+        <label>
+          Enter User ID (1-12)
+          <input
+            type="text"
+            id="userId"
+            name="userId"
+            required
+            onChange={onValueChanged}
+            value={userIdValue}
+            disabled={isFetching}
+          />
+        </label>
+        <input
+          type="submit"
+          value="Fetch"
+          onClick={onFetchClicked}
+          disabled={isFetching}
+        />
+        {isFetching && (
+          <label className="status">
+            Fetching data. Please wait (max wait: 5 seconds)...
+          </label>
+        )}
+        {!isSuccessful && errorMessage.length > 0 && (
+          <label className="error">{errorMessage}</label>
+        )}
+      </form>
+      {isSuccessful && (
+        <div className="result">
+          <h2>Result</h2>
+
+          <pre>
+            <code>{result}</code>
+          </pre>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
